6d14c45c08c15d1471dca491a7a0df5b99977435
André L. C. Moreira <alcm99@gmail.com>
2020-04-05 14:17:14 -0300

    [robot] stable version of example2

--- robot/src/ex2/lib/PingLib.py
+++ robot/src/ex2/lib/PingLib.py
@@ -1,66 +1,154 @@
 from robot.api.deco import keyword
 from json import load
-from os import system
+from paramiko import SSHClient, AutoAddPolicy
 from docker import from_env
 from docker.errors import NotFound
+from re import search
 
 class PingLib(object):
 
-    """"""
+    """Implementation of PingLib used on ping tool tests."""
 
-    #def __init__(self, cfg_file_path):
-    #    """"""
-    #    self.__containers = {}
-
-    #    try:
-    #        with open(cfg_file_path, 'r') as cfg_file:
-    #            self.__cfg_data = load(cfg_file)
-    #    except Exception as e:
-    #        raise e
-
-    @keyword("I ${option} a host with address ${host_ip} on network")
-    def check_host(self, option, host_ip):
+    def __init__(self, cfg_file):
         """
-        Check if a given host is online or offline on network.
+        Initialize object attributes.
 
         Args:
-            option (str): Must be 'have' (when we want to check if a host is available) or
-            'do not have' (when we check if the host is not available).
-            host_ip (str): Ip of host.
+            cfg_file (str): Name of config file.
+
+        Raises:
+            FileNotFoundError: When the specified doesn't exist.
+        """
+        try:
+            self.__ping_output = ''
+            self.__containers = {}
+            self.__ssh_clients = {}
+
+            with open(cfg_file) as file:
+                self.__topology = load(file)
+        except FileNotFoundError as e:
+            raise e
+
+    @keyword("The container ${name} is ${state}")
+    def check_container_state(self, name, state):
+        """
+        Check the operational state of a given container.
+
+        Args:
+            name (str): Name of container.
+            state (str): The operational state of container (up or down).
+
         Raises:
             AssertionError: When an assert error ocurrs.
         """
-        cmd = 'ping -c 2 ' + host_ip
-        ret = system(cmd)
+        client = self.__containers[name]['client']
 
-        if option == "have" and ret == 256:
-            raise AssertionError("The specified address is offline!")
-        elif option == 'do not have' and ret == 0:
-            raise AssertionError("The specified address is online!")
+        for container in client.containers():
+            if container['Names'][0] == ('/' + name):
+                if state == 'down' and container['State'] == 'running':
+                    raise AssertionError('The container {} is up!'.format(name))
+                if state == 'up' and container['State'] != 'running':
+                    raise AssertionError('The container {} is down!'.format(name))
 
-    @keyword("I send ${pkt_count} ${pkg_type} packets to ${hostname_2} from ${hostname_1} using the ping tool")
-    def ping(self, pkt_count, pkt_type, hostname_2, hostname_1):
-        """"""
-        pass
+    @keyword("I send ${pkt_count} packets to ${hostname_2} from ${hostname_1} using the ping tool")
+    def ping(self, pkt_count, hostname_2, hostname_1):
+        """
+        Send a given number of packets from the host 'hostname_1' to host 'hostname_2' using ping.
 
-    @keyword("The host ${hostname_1} must receive ${pkt_count} ${pkt_type} packets from ${hostname_2}")
-    def must_receive(self, hostname_1, pkt_count, pkt_type, hostname_2):
-        """"""
-        pass
+        Args:
+            pkt_count (str): Number of packets to be send.
+            hostname_2 (str): Name of target host.
+            hostname_1 (str): Name of source host.
+        """
+        ip = self.__topology[hostname_2]['ip']
 
-    @keyword('Create a container named ${host1} with image ${image}')
-    def create_container(self, name, image):
-        pass
-        #try:
-        #    client = from_env()
-        #    client.inspect_image('ubuntu-demo') # check if the image exists.
-        #    db_container = client.create_container('ubuntu-demo')
+        client = self.__ssh_clients[hostname_1]
+        _,stdout,_ = client.exec_command('ping -c ' + pkt_count + ' ' + ip)
+        self.__ping_output = stdout.read().decode('ascii')
 
-        #    client.start(container=db_container.get('Id')) # setup
+    @keyword("The host ${hostname_1} must receive ${pkt_count} reply packets from ${hostname_2}")
+    def must_receive(self, hostname_1, pkt_count, hostname_2):
+        """
+        Check if a given number of packets was received from 'hostname_2' to 'hostname_1'.
 
-        #except NotFound:
-        #    raise RuntimeError("the container image wasn't found, aborting the execution!")
+        Args:
+            hostname_1 (str): Name of source host.
+            pkt_count (str): Number of packets to be send.
+            hostname_2 (str): Name of target host.
 
-    @keyword('Destroy the container ${name}')
+        Raises:
+            AssertionError:
+        """
+        ip = self.__topology[hostname_2]['ip']
+
+        for i in range(1, int(pkt_count) + 1):
+            pattern = '64 bytes from %s: icmp_seq=%d ttl=\d\d time=\d.\d\d\d ms\n' % (ip, i)
+
+            if search(pattern, self.__ping_output) is None:
+                raise AssertionError('pattern not found!')
+
+    @keyword('I create a container named ${container_name} with image ${image}')
+    def create_container(self, container_name, image):
+        """
+        Create a new container using the specified image.
+
+        Args:
+            container_name (str): The container name.
+            image (str): The image name used to build the container.
+
+        Raises:
+            RunTimeError: When the specified image doesn't exist.
+        """
+        try:
+            client = from_env()
+            client.inspect_image('ubuntu-demo') # check if the image exists.
+            demo_container = client.create_container('ubuntu-demo', name=container_name)
+
+            client.start(container=demo_container.get('Id'))
+
+            self.__containers[container_name] = { 'client': client, 'container': demo_container }
+        except NotFound:
+            raise RuntimeError("the container image wasn't found, aborting the execution!")
+
+    @keyword('I destroy the container ${name}')
     def destroy_container(self, name):
-        pass
+        """
+        Destroy an existing container.
+
+        Args:
+            name (str): Name of container to be destroyed.
+        """
+        client = self.__containers[name]['client']
+        demo_container = self.__containers[name]['container']
+
+        client.stop(container=demo_container.get('Id'))
+        client.remove_container(container=demo_container.get('Id'))
+
+    @keyword('I connect via ssh to ${hostname}')
+    def connect_ssh(self, hostname):
+        """
+        Open a ssh connection to a given host.
+
+        Args:
+            hostname (str): Name of host.
+        """
+        ip = self.__topology[hostname]['ip']
+        user = self.__topology[hostname]['user']
+        pswd = self.__topology[hostname]['pass']
+
+        client = SSHClient()
+        client.set_missing_host_key_policy(AutoAddPolicy())
+        client.connect(ip, username=user, password=pswd)
+
+        self.__ssh_clients[hostname] = client
+
+    @keyword('I close the ssh connection with ${hostname}')
+    def disconnect_ssh(self, hostname):
+        """
+        Close the ssh connection to a given host.
+
+        Args:
+            hostname (str): Name of host.
+        """
+        client = self.__ssh_clients[hostname]
+        client.close()
--- robot/src/ex2/tests/ping.robot
+++ robot/src/ex2/tests/ping.robot
@@ -4,69 +4,43 @@ Documentation  Suppose we are implementing a tool like ping, who the main goal i
 ...            connectivity between two hosts. This suite contains the implementation of the tests
 ...            related to this tool.
 
-Library        SSHLibrary
-Library        ../lib/PingLib.py
+Library        ../lib/PingLib.py    ${cfg_file}
 
 Suite Setup     Suite setup
 Suite Teardown  Suite teardown
-#Test Setup      Test setup
-#Test Teardown   Test teardown
+Test Setup      Test setup
+Test Teardown   Test teardown
 
 *** Variables ***
 
-${user}      host
-${pass}      1234
-
-${host1_ip}  172.17.0.4
-${host2_ip}  172.17.0.5
-${host3_ip}  172.17.0.7
-${host5_ip}  172.17.0.8
-
-#${cfg_file}     topology.json
-${pkt_count}    10
-#${host1}        host1
-#${host2}        host2
+${pkt_count}    5
 
 *** Test cases ***
 
 Ping a reachable host
     [Documentation]   This test validates the scenario where the 'host1' run the ping tool against
     ...               a reachable host 'host2'.
-    Given I have a host with address "${host1_ip}" on network
-    Given I have a host with address "${host2_ip}" on network
-
-    #When I send "${pkt_count}" "echo request" packets to "${host2}" from "${host1}" using the ping tool
-    #Then The host "${host2}" must receive "${pkt_count}" "echo request" packets from "${host1}"
-    #And The host "${host1}" must receive "${pkt_count}" "echo reply" packets from "${host2}"
-
-#Ping an unreachable host
-#    [Documentation]   This test validates the scenario where the 'host1' run the ping tool against
-#    ...               an unreachable host 'host2'.
-#    Given I "have" a host with address "${host1_ip}" on network
-#    Given I "do not have" a host with address "${host2_ip}" on network
-
-    #When I send "${pkt_count}" "echo request" packets to "${host2}" from "${host1}" using the ping tool
-    #Then The host "${host1}" must receive "0" "echo reply" packets from "${host2}"
+    When I send ${pkt_count} packets to host2 from host1 using the ping tool
+    Then The host host1 must receive ${pkt_count} reply packets from host2
 
 *** Keywords ***
 
 Suite setup
-    Create a container named "host1" with image "ubuntu-demo"
-    Create a container named "host2" with image "ubuntu-demo"
-    Open Connection     ${host1_ip}
-    Login               ${user}     ${pass}
-    Open Connection     ${host2_ip}
-    Login               ${user}     ${pass}
+    Given I create a container named host1 with image "ubuntu-demo"
+    Then The container host1 is up
+    Given I create a container named host2 with image "ubuntu-demo"
+    Then The container host2 is up
 
 Suite teardown
-    Destroy the container "host-1"
-    Destroy the container "host-2"
-    Close All Connections
+    Given I destroy the container host1
+    Given I destroy the container host2
+    And The container host1 is down
+    And The container host2 is down
 
-#Test setup
-#    # Start tcpdump on both machines
-#    # Remove generated pcap files
-#
-#Test teardown
-#    # Stop tcpdump on both machines
-#    # Remove generated pcap files
+Test setup
+    I connect via ssh to host1
+    I connect via ssh to host2
+
+Test teardown
+    I close the ssh connection with host1
+    I close the ssh connection with host2
